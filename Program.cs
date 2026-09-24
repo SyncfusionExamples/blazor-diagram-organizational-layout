db0517843b5aa39791775fff3b431048d3f64b90
Suganthi963 <suganthi.karuppannan@syncfusion.com>
2026-09-24 19:51:44 +0530

    876452: Updated sample.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-using layout;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using OrganizationalChart;
 using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
